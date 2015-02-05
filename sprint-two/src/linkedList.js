68456f165832e13e41da5d3f1f99a96a71beb076
DannyDelott <dannydelott@gmail.com>
2015-02-05 11:40:44 -0800

    Complete linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+
+    var node = Node(value);
+
+    // if empty list
+    if(!list.head && !list.tail){
+      list.head = node;
+      list.tail = node;
+    } else { //else if elements exist in the list
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
+  //come back to this for border cases
+  // empty list? single element (next = null)?
   list.removeHead = function(){
+    var removed = list.head;
+    var newHead = list.head.next;
+    delete list.head;
+    list.head = newHead;
+    return removed.value;
   };
 
   list.contains = function(target){
+    var hasTarget = false;
+    var currentNode = list.head;
+
+    while(currentNode && !hasTarget){
+      if(currentNode.value === target){
+         hasTarget = true;
+      }else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    return hasTarget;
   };
 
   return list;
