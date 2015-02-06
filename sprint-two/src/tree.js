20b836168f6eb41d5d78a6e6bfa66f95015b33b7
DannyDelott <dannydelott@gmail.com>
2015-02-05 16:00:46 -0800

    Complete tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,35 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
+  newTree.children = [];
   // your code here
-  newTree.children = null;  // fix me
+  //
+  // don't forget the _.extend
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target){
+  var hasTarget = false;
 
+  if (this.value === target) {
+    hasTarget = true;
+  }else if (this.children.length > 0) {
+    _.each(this.children, function(child) {
+      if(child.contains(target)){
+        hasTarget = true;
+      }
+    });
+  }
+  return hasTarget;
 };
 
 
