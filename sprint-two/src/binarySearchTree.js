79f102d73c734ae369860ed519c1003cf7729638
DannyDelott <dannydelott@gmail.com>
2015-02-06 16:39:29 -0800

    start binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,45 @@
 var BinarySearchTree = function(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+  var newBST = new BinarySearchTree(value);
+  if (value < this.value) {
+    //belongs on the left
+    if (this.left === null) {
+      this.left = newBST;
+    } else {
+      //recuse on next BST node if left isn't null
+      this.left.insert(value);
+    }
+  } else if (value > this.value) {
+    //belongs on the right
+    if (this.right === null) {
+      this.right = newBST;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+
+BinarySearchTree.prototype.contains = function(value){
+  return true;
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb){
+
+};
+
+//worth hi-chews
+//breadthFirstLog
+
+
+//worth lots of hi-chews
+//rebalanceTree
 
 /*
  * Complexity: What is the time complexity of the above functions?
