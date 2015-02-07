5ac97e88bdfce758e05ae347bf55359e851fabc4
DannyDelott <dannydelott@gmail.com>
2015-02-06 16:07:04 -0800

    complete hash table data structure

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,27 +3,28 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
+// insert method:
+// hashes key to get the bucket index
+// creates tuple, stores in bucket
+// profit!
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this.retrieve(k);
+  var bucket = this._storage.get(i);
   if(!Array.isArray(bucket) || bucket === null){
     bucket = [];
   }
-  //is there a bucket at this index?
-  //no?
-  //  make a bucket
-  //put tuple into bucket
-  //profit!
   var tuple = [k, v];
   bucket.push(tuple);
-
-  // this._storage.set(i, tuple);
   this._storage.set(i, bucket);
 };
 
+// retrieve method:
+// hashes key to get bucket index
+// iterates over tuples in bucket to return value
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
+
   var retrieved = null;
   _.each(bucket, function(tuple, key, collection){
     if(tuple[0] === k){
@@ -34,21 +35,17 @@ HashTable.prototype.retrieve = function(k){
 
 };
 
+// remove method:
+// hashes key to get desired bucket index
+// iterates through tuples in bucket, removing tuple
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
   _.each(bucket, function(tuple, key, collection){
     if(tuple[0] === k){
-
+      bucket.splice(key, 1); // our tuple
     }
   });
- // this._storage.each(this._storage[i], function(item, key, collection){
- //   if(key === i){
-
- //   }
- // });
-  this._storage.set(i, null);
-  //we're deleting whole tuple here
 };
 
 
