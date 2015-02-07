32c3a015030232ba4cf23e84f1653e6313e9e504
DannyDelott <dannydelott@gmail.com>
2015-02-06 18:56:18 -0800

    complete binary search tree data structure

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -5,11 +5,10 @@ var BinarySearchTree = function(value){
 };
 
 BinarySearchTree.prototype.insert = function(value){
-  var newBST = new BinarySearchTree(value);
   if (value < this.value) {
     //belongs on the left
     if (this.left === null) {
-      this.left = newBST;
+      this.left = new BinarySearchTree(value);
     } else {
       //recuse on next BST node if left isn't null
       this.left.insert(value);
@@ -17,7 +16,7 @@ BinarySearchTree.prototype.insert = function(value){
   } else if (value > this.value) {
     //belongs on the right
     if (this.right === null) {
-      this.right = newBST;
+      this.right = new BinarySearchTree(value);
     } else {
       this.right.insert(value);
     }
@@ -26,12 +25,38 @@ BinarySearchTree.prototype.insert = function(value){
 
 
 BinarySearchTree.prototype.contains = function(value){
-  return true;
+  // looks similar to .insert(); maybe refactor later
+  var hasValue = false;
+  if (this.value === value) {
+    hasValue = true;
+  } else if (value < this.value) {
+    //search on the left
+     if (this.left === null) { //need to do a null check
+      return false;
+    } else {
+      //recuse on next BST node if left isn't null
+      hasValue = this.left.contains(value);
+    }
+  } else if (value > this.value) {
+    //search on the right
+    if (this.right === null) {
+      return false;
+    } else {
+      hasValue = this.right.contains(value);
+    }
+  }
+  return hasValue;
 
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(cb){
-
+  cb(this.value);
+  if(this.left !== null){
+    this.left.depthFirstLog(cb);
+  }
+  if(this.right !== null){
+    this.right.depthFirstLog(cb);
+  }
 };
 
 //worth hi-chews
